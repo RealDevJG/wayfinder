f2a31d1bab5048ab613342f71ad0032e40f14129
andrew georgiou <andrew.georgiou03@gmail.com>
2026-01-14 19:08:27 +0000

    removed testing things

--- src/utilities/sizingUtils.ts
+++
@@ -1,6 +0,0 @@
-import { PixelRatio } from "react-native";
-
-// export function normaliseFontSize(size: number, width: number) {
-// 	const scale = width / 375;
-// 	return Math.round(PixelRatio.roundToNearestPixel(size * scale));
-// }
